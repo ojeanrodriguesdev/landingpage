64d100c04fdfe117d3d558e542e4e3a855be7cf1
ojeandev <ojeanrodriguesdev@gmail.com>
2024-10-02 00:35:16 +0200

    CTASectionOne

--- src/app/components/view/CTASection.tsx
+++ src/app/components/view/CTASection.tsx
@@ -6,7 +6,7 @@ export default function CTASection() {
       <div className="overflow-hidden bg-white dark:bg-black lg:mx-8 lg:flex lg:max-w-6xl lg:w-full lg:shadow-md lg:rounded-xl">
         <div className="lg:w-1/2">
           <Image
-            src="/"
+            src="/images/CTASection/image.png"
             alt="Background Image"
             width={1470}
             height={980}
@@ -19,7 +19,7 @@ export default function CTASection() {
 
         <div className="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
           <h2 className="text-[2xl] font-bold text-gray-800 dark:text-white md:text-[24px]">
-            Está pronto para sua <span className="text-blue-500">transformação</span> digital?
+            Está pronto para sua <span className="text-blue-600">transformação</span> digital?
           </h2>
 
           <p className="mt-4 text-gray-500 dark:text-gray-300 font-semibold">
@@ -30,9 +30,9 @@ export default function CTASection() {
           <div className="inline-flex w-full mt-6 sm:w-auto">
             <a
               href="#"
-              className="inline-flex items-center justify-center w-full px-6 font-bold py-2 text-sm text-white duration-300 bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+              className="inline-flex items-center justify-center w-full px-6 font-bold py-2 text-sm text-white duration-300 bg-blue-700 rounded-lg hover:bg-blue-900 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
             >
-              Descubra como
+              Descubra como!
             </a>
           </div>
         </div>    
