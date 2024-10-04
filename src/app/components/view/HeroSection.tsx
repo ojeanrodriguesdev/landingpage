7de5c5796164fa5df7c5a094261a905d002202e7
ojeandev <ojeanrodriguesdev@gmail.com>
2024-10-04 02:16:25 +0200

    HeroSection: Finished All Components

--- src/app/components/view/HeroSection.tsx
+++ src/app/components/view/HeroSection.tsx
@@ -8,16 +8,16 @@ export default function HeroSection() {
       <div className="relative isolate px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-[28px] md:text-[58px] lg:text-[] font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-[28px] md:text-[58px] lg:text-[58px] font-bold tracking-tight text-gray-900 sm:text-6xl">
               A Revolução <span className="text-blue-600">Digital</span> que o Seu{' '}
               <span className="text-blue-600">Negócio</span> Esperava!
             </h1>
-            <h2 className="mt-6 text-[18px] lg:text-[28px] font-semibold leading-8 text-gray-700">
+            <h2 className="mt-6 text-[20px] sm:text-[28px] md:text-[28px] lg:text-[28px] font-semibold leading-8 text-gray-700">
               Simplifique, automatize, expanda seus resultados com nossa solução digital inovadora.
             </h2>
             <div className="flex items-center justify-center">
-              <div className="bg-white w-[300px] md:w-[340px] lg:w-[400px] rounded-[40px] shadow-xl py-6 mt-10 flex flex-col items-center justify-center">
-                <span className="mb-6 text-[14px] lg:text-[18px] font-semibold leading-7 ">
+              <div className="bg-white w-[300px] md:w-[340px] lg:w-[400px] rounded-[20px] shadow-xl py-3 mt-10 flex flex-col items-center justify-center">
+                <span className="mb-3 text-[14px] lg:text-[18px] font-semibold leading-7 ">
                   Transforme seu futuro digital hoje.
                 </span>
                 <a
